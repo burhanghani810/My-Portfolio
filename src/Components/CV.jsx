95e4847f9ef619424dc967f5c36259731e5b5ed8
burhanghani810 <hassistiv205@gmail.com>
2026-09-15 13:19:44 -0700

    updated file type

--- src/Components/CV.jsx
+++ src/Components/CV.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import cvImage from '../assets/Screenshot 2026-08-05 043709.png'
-import cvPdf from '../utils/CV-MuhammadBurhanGhani.docx'
+import cvPdf from '../utils/CV-MuhammadBurhanGhani.pdf'
 
 const CV = () => {
   const [previewOpen, setPreviewOpen] = useState(false)
